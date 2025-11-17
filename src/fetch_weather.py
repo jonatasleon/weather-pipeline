6589c5763e6504c19d40d720f8073975a29dd845
Jonatas Leon <jonatas.leon@gmail.com>
2025-11-17 14:28:46 -0300

    feat: Add weather history fetching functionality
    Introduced a new function, fetch_weather_history, to retrieve historical weather data from the archive API. Updated the existing fetch_weather function to rename it to fetch_weather_forecast and adjusted the API URL for clarity. This enhancement allows users to access both current and historical weather data.

--- src/fetch_weather.py
+++ src/fetch_weather.py
@@ -1,18 +1,40 @@
 import requests
 
 
-API_URL = "https://api.open-meteo.com/v1/forecast"
+FORECAST_API_URL = "https://api.open-meteo.com/v1/forecast"
+ARCHIVE_API_URL = "https://archive-api.open-meteo.com/v1/era5"
 
 
-def fetch_weather(latitude, longitude, timezone: str = "America/Sao_Paulo"):
+def fetch_weather_forecast(latitude, longitude, timezone: str = "America/Sao_Paulo"):
     params = {
         "latitude": latitude,
         "longitude": longitude,
         "hourly": ["temperature_2m", "windspeed_10m", "relative_humidity_2m"],
         "timezone": timezone,
     }
-    res = requests.get(API_URL, params=params)
+    res = requests.get(FORECAST_API_URL, params=params)
     res.raise_for_status()
     data = res.json()
 
+    return data
+
+
+def fetch_weather_history(
+    latitude,
+    longitude,
+    start_date,
+    end_date,
+    timezone: str = "America/Sao_Paulo",
+):
+    params = {
+        "hourly": ["temperature_2m", "windspeed_10m", "relative_humidity_2m"],
+        "latitude": latitude,
+        "longitude": longitude,
+        "start_date": start_date.strftime("%Y-%m-%d"),
+        "end_date": end_date.strftime("%Y-%m-%d"),
+        "timezone": timezone,
+    }
+    res = requests.get(ARCHIVE_API_URL, params=params)
+    res.raise_for_status()
+    data = res.json()
     return data
